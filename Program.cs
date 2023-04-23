06269fac63769987a8510b402cca3d583add225c
Julia <juliet.yor@gmail.com>
2023-04-23 17:02:33 +0300

    add algoritm

--- Program.cs
+++ Program.cs
@@ -1,1 +1,29 @@
-﻿
+﻿string [] array(){
+Console.Write("Какая длина будет у массива? ");
+int size = Convert.ToInt32(Console.ReadLine());
+string [] array = new string[size];
+int len = 3;
+int pos = 0;
+for (int i = 0; i < size; i++)
+{
+    Console.WriteLine($"Введите {i+1}-й элемент: ");
+    string? element = Console.ReadLine();
+    if (element?.Length <= len)
+    {
+        array[pos] = element;
+        pos++;
+    }  
+}
+return array;
+}
+
+PrintArray(array());
+
+void PrintArray(string [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
